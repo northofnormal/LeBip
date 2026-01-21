ae377c06a26ccc526a1db03337eccc1a79c1ac5c
Anne Cahalan <northofnormal@gmail.com>
2026-01-21 10:26:32 -0500

    Initial Commit

---
+++ LeBip/LeBipApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeBipApp.swift
+//  LeBip
+//
+//  Created by Anne Cahalan on 1/21/26.
+//
+
+import SwiftUI
+
+@main
+struct LeBipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LeBipTests/LeBipTests.swift
@@ -0,0 +1,17 @@
+//
+//  LeBipTests.swift
+//  LeBipTests
+//
+//  Created by Anne Cahalan on 1/21/26.
+//
+
+import Testing
+@testable import LeBip
+
+struct LeBipTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
